954d6aa8787356e7fccb84f0f9dee0121ac94356
Michiel-Vanzeir <michiel.vanzeir@gmail.com>
2022-07-15 15:42:38 +0000

    Maintenance

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -8,7 +8,10 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/ubuntu/ros/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/ubuntu/ros/devel/lib:$LD_LIBRARY_PATH"
 export PATH='/opt/ros/noetic/bin:/home/ubuntu/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export PKG_CONFIG_PATH="/home/ubuntu/ros/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/ubuntu/ros/build'
+export PYTHONPATH="/home/ubuntu/ros/devel/lib/python3/dist-packages:$PYTHONPATH"
 export ROSLISP_PACKAGE_DIRECTORIES='/home/ubuntu/ros/devel/share/common-lisp'
 export ROS_PACKAGE_PATH="/home/ubuntu/ros/src:$ROS_PACKAGE_PATH"
